fba3a84d9367a583d8c92e9c42c32d02e647d8c2
Aditya Chopade <adityachopade08@gmail.com>
2026-08-15 17:54:49 +0530

    Encapsulation done

---
+++ OOP/Encapsulation/coex3.cpp
@@ -0,0 +1,77 @@
+// Create:
+// class Rectangle
+
+// Private:
+// int length;
+// int breadth;
+
+// Create setters:
+// void setLength(int length);
+// void setBreadth(int breadth);
+
+// Create getters:
+// int getLength();
+// int getBreadth();
+
+// Rules:
+// Length must be greater than 0.
+// Breadth must be greater than 0.
+
+// Create:
+// int area();
+// Return the area.
+
+#include<iostream>
+using namespace std;
+
+class Rectangle
+{
+private:
+    int length;
+    int breadth;
+
+public:
+    void setLength(int length)
+    {
+        if(length > 0){
+            this->length = length;
+        }
+        else{
+            cout << "Enter valid length";
+        }
+    }
+    void setBreadth(int breadth)
+    {
+        if(breadth > 0){
+            this->breadth = breadth;
+        }
+        else{
+            cout << "Enter valid breadth" ;
+        }
+    }
+
+    int getLength()
+    {
+        return this->length;
+    }
+    int getBreadth()
+    {
+        return this->breadth;
+    }
+
+    int area()
+    {
+        return this->length*this->breadth;
+    }
+};
+
+int main()
+{
+    Rectangle r;
+    r.setLength(20);
+    r.setBreadth(10);
+
+    cout << "Area of rectangle is " << r.area();
+
+    return 0;
+}
