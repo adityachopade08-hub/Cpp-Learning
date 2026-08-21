8eac9f92dbaa27660004fde64a04e040a3087b02
Aditya Chopade <adityachopade08@gmail.com>
2026-08-21 14:50:43 +0530

    vector 2

---
+++ Vectors/coex2.cpp
@@ -0,0 +1,26 @@
+// Create:=
+// vector<int> v = {10,20,30,40,50};
+
+// Print:
+// First element:
+// Last element:
+// Size:
+
+// Expected:
+// First element: 10
+// Last element: 50
+// Size: 5
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main()
+{
+    vector<int> v = {10,20,30,40,50};
+
+    cout << v.front() << endl;
+    cout << v.back() << endl;
+
+    return 0;
+}
