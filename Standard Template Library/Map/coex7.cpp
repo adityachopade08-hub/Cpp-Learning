9db2aee4b9bd273df3236f349ad470babcf63ce0
Aditya Chopade <adityachopade08@gmail.com>
2026-08-24 17:46:50 +0530

    aa

---
+++ Standard Template Library/Map/coex7.cpp
@@ -0,0 +1,46 @@
+// Given:
+// map<int,string> students = {
+//     {101,"Aditya"},
+//     {102,"Sakshi"},
+//     {103,"Atharv"}
+// };
+
+// Search for roll number 102 using:
+// students.find(102)
+
+// If found, print:
+// Student Found
+// Name = Sakshi
+
+// Remember:
+// auto it = students.find(102);
+// and:
+// it->first
+// it->second
+
+#include<iostream>
+#include<map>
+using namespace std;
+
+int main()
+{
+    map<int,string> students = {
+        {101,"Aditya"},
+        {102,"Sakshi"},
+        {103,"Atharv"}
+    };
+
+    auto it = students.find(102) ;
+
+    if(it != students.end())
+    {
+        cout << "Found\n";
+        cout << it->first << "  :  " << it->second << endl;
+    }
+    else
+    {
+        cout << "Not Found\n";
+    }
+
+    return 0;
+}
